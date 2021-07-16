2208d6b5e5432c10a36abc2dcc4043fab208f739
washingmachine <bdomanski@protonmail.com>
2021-07-16 12:54:05 +0200

    Initial commit

--- gunicorn.conf.py
+++
@@ -1,5 +0,0 @@
-bind = "0.0.0.0:5000"
-workers = 10
-worker_class = "gevent"
-keyfile = "/etc/letsencrypt/live/server.duinocoin.com/privkey.pem"
-certfile = "/etc/letsencrypt/live/server.duinocoin.com/fullchain.pem"
