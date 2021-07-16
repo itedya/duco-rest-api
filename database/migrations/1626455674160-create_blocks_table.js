3181f78d2e1682b90d97dd4b703d0b251aed35d8
washingmachine <bdomanski@protonmail.com>
2021-07-16 19:26:00 +0200

    Created Blocks migration

---
+++ database/migrations/1626455674160-create_blocks_table.js
@@ -0,0 +1,40 @@
+const { Table } = require("typeorm");
+
+module.exports = class createBlocksTable1626455674160 {
+
+    async up(queryRunner) {
+        queryRunner.createTable(new Table({
+            name: "blocks",
+            columns: [
+                {
+                    name: "hash",
+                    type: "varchar",
+                    max: 40,
+                    isNullable: false
+                },
+                {
+                    name: "timestamp",
+                    type: "datetime",
+                    isNullable: false
+                },
+                {
+                    name: "finder",
+                    type: "varchar",
+                    max: 320,
+                    isNullable: false
+                },
+                {
+                    name: "amount",
+                    type: "real",
+                    isNullable: false,
+                    unsigned: true
+                }
+            ]
+        }), true)
+    }
+
+    async down(queryRunner) {
+        queryRunner.dropTable("")
+    }
+}
+        
