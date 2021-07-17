8ad6e8fb1c38eafdf59166ba4f2dfee86f53723a
washingmachine <bdomanski@protonmail.com>
2021-07-17 15:56:59 +0200

    Repaired pagination validator

--- app/requests/pagination.js
+++ app/requests/pagination.js
@@ -2,5 +2,5 @@ const { query } = require('express-validator');
 
 module.exports = [
     query('per_page').isInt({min: 5, max: 50}),
-    query('page').isInt({min: 1, max: 999999999})
+    query('page').isInt({min: 0, max: 999999999})
 ]
