31c536b3068756756bcf1a8a8adf47fc0549a8ed
washingmachine <bdomanski@protonmail.com>
2021-07-17 14:04:42 +0200

    Updated method of connection in typeorm.js

--- plugins/typeorm.js
+++ plugins/typeorm.js
@@ -3,10 +3,4 @@ const User = require('./../app/entities/User')
 
 const toAbs = process.cwd();
 
-module.exports = typeorm.createConnections([{
-    name: "default",
-    type: "sqlite",
-    database: `${toAbs}/${process.env.DATABASE_URI_CRYPTO}`,
-    entities: [User],
-    synchronize: true
-}])
+module.exports = typeorm.createConnections()
