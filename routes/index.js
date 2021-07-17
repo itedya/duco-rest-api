64bd6d263d44c9b514584a788c4a71c9fc5fb53c
washingmachine <bdomanski@protonmail.com>
2021-07-18 00:23:20 +0200

    Changed index router

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  return res.json({ result: "OK" })
 });
 
 module.exports = router;
