c4f3950e6b7c1e6c61252e0af2ac82ea261e782a
washingmachine <bdomanski@protonmail.com>
2021-07-18 22:07:19 +0200

    Removed weird comment

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,6 @@ import {Request, Response} from "express";
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', (req: Request, res: Response) => {
   return res.json({ result: "OK" });
 });
